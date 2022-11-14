fca69295e5aa681e1ae90c7d3b707376292d009a
CMR211 <bartosz.surma211@gmail.com>
2022-11-14 15:49:28 +0100

    add: card template, featured page on home

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,14 +1,39 @@
 import React from "react"
-import styles from "../../styles/components/Card.module.scss"
+import Image from "next/image"
+import styles from "../styles/components/Card.module.scss"
+
+import plants from "../server/json/plants.json"
 
 type CardProps = {
+    id: string
+    showOldPrice?: boolean
+}
+
+type PlantType = {
+    id: string
     name: string
     category: string
     price: number
-    oldPrice?: number
-    img: string
+    old_price: number
+    desc: string
+    images: string[]
 }
 
-export default function Card({ name, category, price, oldPrice = -1, img }: CardProps) {
-    return <div className={styles.card}>Card</div>
+export default function Card({ id, showOldPrice }: CardProps) {
+    const plant: PlantType = plants.find((plant) => plant.id === id)
+    const price = plant.price.toLocaleString("en-US", { style: "currency", currency: "USD" })
+    const oldPrice = plant.old_price.toLocaleString("en-US", { style: "currency", currency: "USD" })
+    return (
+        <div className={styles.card}>
+            <img className={styles.img} width={280} height={280} src={plant?.images[0]} alt={`${plant?.name} alt`} />
+            <p className={styles.name}>{plant?.name}</p>
+            <p className={styles.price}>
+                {showOldPrice ? <span className={styles.old_price}>{oldPrice}</span>:null}
+                <span className={styles.price_prefix}>{price.slice(0,1)}</span>
+                {price.slice(1,-3)}
+                <span className={styles.price_suffix}>{price.slice(-3)}</span>
+            </p>
+            <p className={styles.category}>{plant?.category}</p>
+        </div>
+    )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import type { NextPage } from "next"
 import Head from "next/head"
-import Image from "next/image"
-import Card from "../components/Card"
+
 import HomeButton from "../components/Home/HomeButton"
 import Navbar from "../components/Navbar/Navbar"
+import HotNow from "../components/Home/HotNow"
 import styles from "../styles/pages/Home.module.scss"
 
 export default function Home() {
@@ -20,21 +20,22 @@ export default function Home() {
                 <section className={styles.section}>
                     <div className={styles.container}>
                         <h1 className={styles.title}>
-                            <span>The</span><br />Plants<br />Store
+                            <span>The</span>
+                            <br />
+                            Plants
+                            <br />
+                            Store
                         </h1>
                         <p className={styles.subtitle}>We love plants like you do</p>
                         <p className={styles.desc}>
                             Explore our growing collection of plants, wholeheartedly selected and maintained with care by us -
-                            plant lovers. 
+                            plant lovers.
                         </p>
                         <p className={styles.desc}>Every plant, even exotic ones, available at your home in just 2 days.</p>
                         <HomeButton text="Go shopping" />
                     </div>
                 </section>
-                <section>
-                    <h2>Hot now</h2>
-                    {/* <Card /> */}
-                </section>
+                <HotNow />
             </main>
         </>
     )
