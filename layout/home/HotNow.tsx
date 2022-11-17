0723fe2d30cde5c3d2666c32f6f7d77b166b08a0
CMR211 <bartosz.surma211@gmail.com>
2022-11-17 15:56:45 +0100

    update: featured section layout change

--- layout/home/HotNow.tsx
+++ layout/home/HotNow.tsx
@@ -5,13 +5,15 @@ import Card from "../components/Card"
 export default function HotNow() {
     return (
         <section className={styles.section}>
-            <h2 className={styles.title}>Featured special offers</h2>
-            <div className={styles.bg}></div>
-            <div className={styles.container}>
-                <Card id="001" showOldPrice={true} />
-                <Card id="002" showOldPrice={true} />
-                <Card id="003" showOldPrice={true} />
-                <Card id="004" showOldPrice={true} />
+            <div className={styles.wrapper}>
+                <h2 className={styles.title}>Featured special offers</h2>
+                <div className={styles.bg}></div>
+                <div className={styles.container}>
+                    <Card id="001" showOldPrice={true} />
+                    <Card id="002" showOldPrice={true} />
+                    <Card id="003" showOldPrice={true} />
+                    <Card id="004" showOldPrice={true} />
+                </div>
             </div>
         </section>
     )
