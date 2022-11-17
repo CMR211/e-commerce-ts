3af2730061e7d356a2ef9e0f1894bd8fb118b165
CMR211 <bartosz.surma211@gmail.com>
2022-11-17 15:56:07 +0100

    add: features

---
+++ layout/home/Features.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Features() {
+  return (
+    <div className={styles.section}>
+        <h2 className={styles.title}>Features</h2>
+        <div className={styles.container}
+    </div>
+  )
+}
