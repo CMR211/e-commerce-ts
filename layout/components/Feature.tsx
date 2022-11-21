3d39d4768e63a40e22c3d000b326e47d1033d3c2
CMR211 <bartosz.surma211@gmail.com>
2022-11-21 13:13:23 +0100

    fix: ts error

--- layout/components/Feature.tsx
+++ layout/components/Feature.tsx
@@ -10,7 +10,7 @@ type FeatureType = {
 export default function Feature({ title, description, imageUrl }: FeatureType) {
     return (
         <div className={styles.container}>
-            <div className={styles.bg} style={{ "background-image": `url(${imageUrl}` }}></div>
+            <div className={styles.bg} style={{ backgroundImage: `url(${imageUrl}` }}></div>
             <div className={styles.content}>
                 <h3 className={styles.title}>{title}</h3>
                 <p className={styles.desc}>{description}</p>
