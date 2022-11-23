265bf6727d556338a521e521771fe55d1ea459b0
CMR211 <bartosz.surma211@gmail.com>
2022-11-23 16:24:18 +0100

    update: mongodb connection, GET /plants, /POST /plant methods

--- server/server.js
+++ server/server.js
@@ -4,18 +4,19 @@ require("dotenv").config()
 
 const database = require("./database")
 
+app.use(express.json())
+
 app.get("/plants", async (req, res) => {
     const json = await database.load("plants")
-    console.log(json)
     res.json(json)
     res.end()
 })
 
-app.post("/plant", async (req,res) => {
-	const data = req.body
-	console.log(data)
-	res.end()
-	// const addedDocument = await database.create("plants", )
+app.post("/plant", async (req, res) => {
+    const json = req.body
+    const addedDocument = await database.create("plants", json)
+    res.json(addedDocument)
+    res.end()
 })
 
 app.listen(3000, () => console.log("server is running"))
