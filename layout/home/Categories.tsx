a46ced00f8f949b285d08542ce9969bbdbf05bc2
CMR211 <bartosz.surma211@gmail.com>
2022-11-23 13:50:07 +0100

    refactor: moved categories to standalone json file

--- layout/home/Categories.tsx
+++ layout/home/Categories.tsx
@@ -1,15 +1,16 @@
 import React from "react"
 import styles from "./Categories.module.scss"
 import Category from "../components/Category"
+import categories from "../json/categories.json"
 
 export default function Categories() {
     return (
         <div className={styles.container}>
             <h2 className={styles.title}>Browse categories</h2>
             <div className={styles.categories}>
-                <Category name="Araceae" />
-                <Category name="Apocynaceae" />
-                <Category name="Euphorbiaceae" />
+                {categories.map((category) => (
+                    <Category key={category} name={category} />
+                ))}
             </div>
         </div>
     )
