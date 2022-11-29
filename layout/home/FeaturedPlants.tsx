e920ff37f011c64db5e0b57ce1b6aa3c19b16c04
CMR211 <bartosz.surma211@gmail.com>
2022-11-29 13:16:28 +0100

    update: featured plants refactor

--- layout/home/FeaturedPlants.tsx
+++ layout/home/FeaturedPlants.tsx
@@ -1,18 +1,26 @@
 import React from "react"
 import styles from "./FeaturedPlants.module.scss"
 import Card from "../components/Card"
+import { useDiscountedPlants } from "../../utils/useApiPlant"
+import { Plant } from "../../interfaces/plant"
 
 export default function FeaturedPlants() {
+    const { data, error }: { data: Plant[]; error: string } = useDiscountedPlants()
+
+    if (error) return <p>Failed to get discounted plants</p>
+    if (!data) return <p>Loading...</p>
+
+    const discountedPlantsIds = data.map((plant) => plant.id)
+
     return (
         <section className={styles.section}>
             <div className={styles.wrapper}>
                 <h2 className={styles.title}>Featured special offers</h2>
                 <div className={styles.bg}></div>
                 <div className={styles.container}>
-                    <Card id="63762ab9c0bc3e20323e48f3" showOldPrice={true} />
-                    <Card id="002" showOldPrice={true} />
-                    <Card id="003" showOldPrice={true} />
-                    <Card id="004" showOldPrice={true} />
+                    {discountedPlantsIds.map((plantID) => (
+                        <Card key={plantID} id={plantID} showOldPrice={true} />
+                    ))}
                 </div>
             </div>
         </section>
